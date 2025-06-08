6af0dc30af7e82ca9caee8bd3c0efd749b8e2cec
Truong Dien <dienrengoku148@gmail.com>
2025-06-08 22:44:12 +0700

    update code 2

--- src/app/api/product/[productId]/route.ts
+++ src/app/api/product/[productId]/route.ts
@@ -2,18 +2,14 @@ import cloudinary from "../../../../../utils/cloudinary";
 import { connectDB } from "../../db/connectDB";
 import Product from "../../models/product.model";
 
-export async function GET( { params }: { params: Promise<{ productId: string }> }) {
+export async function GET({ params }: { params: Promise<{ productId: string }> }) {
     await connectDB();
     const productId = (await params).productId;
-    try {
-        const product = await Product.findById(productId);
-        if (!product) {
-            return Response.json({ message: "Product not found" }, { status: 400 });
-        }
-        return Response.json({ product }, { status: 200 });
-    } catch (error: any) {
-        return Response.json({ message: error.message }, { status: 400 });
+    const product = await Product.findById(productId);
+    if (!product) {
+        return Response.json({ message: "Product not found" }, { status: 400 });
     }
+    return Response.json({ product }, { status: 200 });
 }
 
 export async function DELETE({ params }: { params: Promise<{ productId: string }> }) {
